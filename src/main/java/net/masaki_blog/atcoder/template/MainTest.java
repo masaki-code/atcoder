ea35d5a19bf49f5fac2e78b52a10d7634e0ca899
masaki <masaki@masaki-blog.net>
2021-02-06 19:06:59 +0900

    modified template

--- src/main/java/net/masaki_blog/atcoder/template/MainTest.java
+++ src/main/java/net/masaki_blog/atcoder/template/MainTest.java
@@ -1,12 +1,5 @@
 package net.masaki_blog.atcoder.template;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.*;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.params.provider.Arguments;
@@ -19,28 +12,8 @@ class MainTest extends MainTestBase {
 	private static final String OUTPUT_FILE = "output.txt";
 
 	static List<Arguments> dataProvider() {
-		try {
+		return new MainTest().getDataProvider(INPUT_FILE, OUTPUT_FILE);
 
-			String inputFile = MainTest.class.getResource(INPUT_FILE).getPath();
-			String outputFile = MainTest.class.getResource(OUTPUT_FILE).getPath();
-
-			List<String> inputList = Files.readAllLines(Paths.get(inputFile));
-			List<String> outputList = Files.readAllLines(Paths.get(outputFile));
-
-			List<Arguments> pattern = new ArrayList<>();
-
-			for (int i = 0; i < inputList.size(); i++) {
-				String input = inputList.get(i);
-				Object[] inputArray = input.split(",");
-				String output = outputList.get(i);
-				pattern.add(arguments(input(inputArray), output(output)));
-			}
-
-			return pattern;
-		} catch (IOException e) {
-			fail(e);
-			throw new RuntimeException(e);
-		}
 	}
 
 }
