27d01f4e5bb191ba817a8163854b96b0b27a4a72
masaki <masaki@masaki-blog.net>
2021-02-20 01:13:19 +0900

    unused

--- src/main/java/net/masaki_blog/atcoder/abc170/abc170_e4/Main.java
+++ src/main/java/net/masaki_blog/atcoder/abc170/abc170_e4/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+@SuppressWarnings("unused")
 public class Main {
 
     public static void main(String... args) throws Exception {
