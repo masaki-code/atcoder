f3175aa9b27e97bded18a0f2ce768236954c2ac4
masaki <masaki@masaki-blog.net>
2021-02-20 23:29:57 +0900

    template

--- src/main/java/net/masaki_blog/atcoder/template/Main.java
+++ src/main/java/net/masaki_blog/atcoder/template/Main.java
@@ -1,30 +1,68 @@
 package net.masaki_blog.atcoder.template;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+@SuppressWarnings("unused")
 public class Main {
 
     public static void main(String... args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        pw.println(new Main(br).execute());
+        new Main().execute();
+    }
+
+    BufferedReader br;
+    PrintWriter pw;
+
+    Main() {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+        this.pw = new PrintWriter(System.out);
+    }
+
+    void execute() throws Exception {
+        Input input = new Input(br);
+        input.readXXXX();
+        input.readYYYY();
         pw.flush();
-
     }
 
-    final int a;
+    private static class Input {
 
-    Main(BufferedReader br) throws Exception {
+        final int n, q;
+        final BufferedReader br;
 
-        String s = br.readLine();
-        this.a = Integer.parseInt(s);
+        Input(BufferedReader br) throws IOException {
+            this.br = br;
+            this.n = readInt(br);
+            this.q = readInt(br);
+        }
 
-    }
+        void readXXXX() throws IOException {
+            for (int i = 0; i < n; i++) {
+                int a = readInt(br);
+                int b = readInt(br);
+            }
+        }
 
-    Object execute() {
-        return "";
+        void readYYYY() throws IOException {
+            for (int i = 0; i < q; i++) {
+                int a = readInt(br);
+                int b = readInt(br);
+            }
+        }
+
+        int readInt(BufferedReader br) throws IOException {
+            int a = 0;
+            while (true) {
+                int read = br.read();
+                if (48 <= read && read <= 57) {
+                    a = a * 10 + read - 48;
+                } else {
+                    return a;
+                }
+            }
+        }
     }
 
 }
