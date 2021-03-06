86e36fe2fa85f91c14de18bab7d0840ca9a78916
masaki <masaki@masaki-blog.net>
2021-03-06 23:58:18 +0900

    abc172_a

---
+++ src/main/java/net/masaki_blog/atcoder/abc172/abc172_a/Main.java
@@ -0,0 +1,40 @@
+package net.masaki_blog.atcoder.abc172.abc172_a;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        new Main().execute();
+    }
+
+    BufferedReader br;
+    PrintWriter pw;
+
+    Main() {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+        this.pw = new PrintWriter(System.out);
+    }
+
+    void execute() throws Exception {
+        int a = readInt();
+        pw.println(a + a * a + a * a * a);
+        pw.flush();
+    }
+
+    int readInt() throws IOException {
+        int a = 0;
+        while (true) {
+            int read = br.read();
+            if (48 <= read && read <= 57) {
+                a = a * 10 + read - 48;
+            } else {
+                return a;
+            }
+        }
+    }
+
+}
